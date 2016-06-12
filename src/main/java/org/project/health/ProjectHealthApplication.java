c7931758a04d2a49c9ea329303a68826903b5984
abhishek <abhishek.choudhary@outlook.in>
2016-06-12 14:17:51 +0530

    intial commit

---
+++ src/main/java/org/project/health/ProjectHealthApplication.java
@@ -0,0 +1,12 @@
+package org.project.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHealthApplication.class, args);
+	}
+}
---
+++ src/main/java/org/project/health/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.project.health.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showHomePage() {
+		return "index";
+	}
+}
